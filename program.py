fcc71f57d7fa2c487ec5c46b93d72951bbb3e98b
Kavinraja.D <119875375+d-kavinraja@users.noreply.github.com>
2023-11-15 07:03:02 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,38 @@
+
+import chardet
+file='/content/spam.csv'
+with open(file,'rb') as rawdata:
+  result = chardet.detect(rawdata.read(100000))
+result
+
+
+import pandas as pd
+data=pd.read_csv('/content/spam.csv',encoding='Windows-1252')
+
+data.head()
+
+data.info()
+
+data.isnull().sum()
+
+x=data["v1"].values
+y=data["v2"].values
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer()
+
+x_train=cv.fit_transform(x_train)
+x_test=cv.transform(x_test)
+
+from sklearn.svm import SVC
+svc=SVC()
+svc.fit(x_train,y_train)
+y_pred=svc.predict(x_test)
+y_pred
+
+from sklearn import metrics
+accuracy=metrics.accuracy_score(y_test,y_pred)
+accuracy
